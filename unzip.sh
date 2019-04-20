d9050ccbd9b9bcfce067df31016dcb3c18a21d8c
none Frontier <liuyiquancov5@gmail.com>
2019-04-20 14:33:46 +0800

    add unzip

---
+++ unzip.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+unzip 3Dircadb1.2.zip
+unzip 3Dircadb1.3.zip
+unzip 3Dircadb1.4.zip
+unzip 3Dircadb1.5.zip
+unzip 3Dircadb1.6.zip
+unzip 3Dircadb1.7.zip
+unzip 3Dircadb1.8.zip
+unzip 3Dircadb1.9.zip
+unzip 3Dircadb1.10.zip
+unzip 3Dircadb1.11.zip
+unzip 3Dircadb1.12.zip
+unzip 3Dircadb1.13.zip
+unzip 3Dircadb1.14.zip
+unzip 3Dircadb1.15.zip
+unzip 3Dircadb1.16.zip
+unzip 3Dircadb1.17.zip
+unzip 3Dircadb1.18.zip
+unzip 3Dircadb1.19.zip
+unzip 3Dircadb1.20.zip
+
+unzip 3Dircadb1.2/PATIENT_DICOM.zip -d 3Dircadb1.2/
+unzip 3Dircadb1.3/PATIENT_DICOM.zip -d 3Dircadb1.3/
+unzip 3Dircadb1.4/PATIENT_DICOM.zip -d 3Dircadb1.4/
+unzip 3Dircadb1.5/PATIENT_DICOM.zip -d 3Dircadb1.5/
+unzip 3Dircadb1.6/PATIENT_DICOM.zip -d 3Dircadb1.6/
+unzip 3Dircadb1.7/PATIENT_DICOM.zip -d 3Dircadb1.7/
+unzip 3Dircadb1.8/PATIENT_DICOM.zip -d 3Dircadb1.8/
+unzip 3Dircadb1.9/PATIENT_DICOM.zip -d 3Dircadb1.9/
+unzip 3Dircadb1.10/PATIENT_DICOM.zip -d 3Dircadb1.10/
+unzip 3Dircadb1.11/PATIENT_DICOM.zip -d 3Dircadb1.11/
+unzip 3Dircadb1.12/PATIENT_DICOM.zip -d 3Dircadb1.12/
+unzip 3Dircadb1.13/PATIENT_DICOM.zip -d 3Dircadb1.13/
+unzip 3Dircadb1.14/PATIENT_DICOM.zip -d 3Dircadb1.14/
+unzip 3Dircadb1.15/PATIENT_DICOM.zip -d 3Dircadb1.15/
+unzip 3Dircadb1.16/PATIENT_DICOM.zip -d 3Dircadb1.16/
+unzip 3Dircadb1.17/PATIENT_DICOM.zip -d 3Dircadb1.17/
+unzip 3Dircadb1.18/PATIENT_DICOM.zip -d 3Dircadb1.18/
+unzip 3Dircadb1.19/PATIENT_DICOM.zip -d 3Dircadb1.19/
+unzip 3Dircadb1.20/PATIENT_DICOM.zip -d 3Dircadb1.20/
+
+
+unzip 3Dircadb1.2/MASKS_DICOM.zip -d 3Dircadb1.2/
+unzip 3Dircadb1.3/MASKS_DICOM.zip -d 3Dircadb1.3/
+unzip 3Dircadb1.4/MASKS_DICOM.zip -d 3Dircadb1.4/
+unzip 3Dircadb1.5/MASKS_DICOM.zip -d 3Dircadb1.5/
+unzip 3Dircadb1.6/MASKS_DICOM.zip -d 3Dircadb1.6/
+unzip 3Dircadb1.7/MASKS_DICOM.zip -d 3Dircadb1.7/
+unzip 3Dircadb1.8/MASKS_DICOM.zip -d 3Dircadb1.8/
+unzip 3Dircadb1.9/MASKS_DICOM.zip -d 3Dircadb1.9/
+unzip 3Dircadb1.10/MASKS_DICOM.zip -d 3Dircadb1.10/
+unzip 3Dircadb1.11/MASKS_DICOM.zip -d 3Dircadb1.11/
+unzip 3Dircadb1.12/MASKS_DICOM.zip -d 3Dircadb1.12/
+unzip 3Dircadb1.13/MASKS_DICOM.zip -d 3Dircadb1.13/
+unzip 3Dircadb1.14/MASKS_DICOM.zip -d 3Dircadb1.14/
+unzip 3Dircadb1.15/MASKS_DICOM.zip -d 3Dircadb1.15/
+unzip 3Dircadb1.16/MASKS_DICOM.zip -d 3Dircadb1.16/
+unzip 3Dircadb1.17/MASKS_DICOM.zip -d 3Dircadb1.17/
+unzip 3Dircadb1.18/MASKS_DICOM.zip -d 3Dircadb1.18/
+unzip 3Dircadb1.19/MASKS_DICOM.zip -d 3Dircadb1.19/
+unzip 3Dircadb1.20/MASKS_DICOM.zip -d 3Dircadb1.20/
+
